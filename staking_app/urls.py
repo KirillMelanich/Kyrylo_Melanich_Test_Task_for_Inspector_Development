432a7e82d1b03185ce786b20a0f5430ac70d086f
KirillMelanich <kirillmelanich@gmail.com>
2023-10-26 20:06:34 +0300

    Create urls for staking_app app

---
+++ staking_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from rest_framework import routers
+
+from .views import UserWalletViewSet, UserPositionViewSet, StackingPoolViewSet, PoolConditionsViewSet
+
+router = routers.DefaultRouter()
+router.register("user_wallet", UserWalletViewSet )
+router.register("user_position", UserPositionViewSet)
+router.register("stacking_pool", StackingPoolViewSet)
+router.register("pool_conditions", PoolConditionsViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "staking_app"
