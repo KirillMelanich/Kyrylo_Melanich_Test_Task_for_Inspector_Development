809ba94552416772c8023390f4f458d800b98cfa
KirillMelanich <kirillmelanich@gmail.com>
2023-10-26 17:04:30 +0300

    Create stacking_app models and register them into admin panel

--- staking_app/apps.py
+++ staking_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StakingAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'staking_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "staking_app"
--- staking_app/models.py
+++ staking_app/models.py
@@ -1,3 +1,42 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserWallet(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=20, decimal_places=5)
+
+    def __str__(self):
+        return f"{self.user.full_name} wallet({self.balance})"
+
+
+class UserPosition(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    cryptocurrency = models.CharField(max_length=50)  # The specific cryptocurrency name
+    amount = models.DecimalField(
+        max_digits=20, decimal_places=5
+    )  # The amount of the cryptocurrency held
+
+    def __str__(self):
+        return f"{self.user.full_name} - {self.cryptocurrency} - {self.amount}"
+
+
+class StackingPool(models.Model):
+    stack_holders = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="stacking_pools", blank=True
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PoolConditions(models.Model):
+    pool = models.ForeignKey(StackingPool, on_delete=models.CASCADE)
+    minimum_stake = models.DecimalField(max_digits=20, decimal_places=5)
+    annual_percentage_yield = models.FloatField()
+
+    def __str__(self):
+        return f"{self.pool.name} - minimum stake - {self.minimum_stake}, annual percentage - {self.annual_percentage_yield}%"
